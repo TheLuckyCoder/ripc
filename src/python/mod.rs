f83b596fe1c5133b051665873776d79fbea9d9ee
Răzvan Filea <razvan.filea@gmail.com>
2025-01-23 17:28:56 +0200

    Delete the CircularQueue

--- src/python/mod.rs
+++ src/python/mod.rs
@@ -1,5 +1,4 @@
-use crate::helpers::bytes::RustBytes;
-use crate::python::circular_queue::PythonSharedCircularQueue;
+use crate::helpers::bytes::RustPyBytes;
 use crate::python::dynamic_queue::PythonSharedQueue;
 use crate::python::message::PythonSharedMessage;
 use crate::python::open_mode::OpenMode;
@@ -8,7 +7,6 @@ use pyo3::types::PyFunction;
 use pyo3::{pymodule, Bound, PyResult};
 use rayon::prelude::*;
 
-mod circular_queue;
 mod dynamic_queue;
 mod message;
 mod open_mode;
@@ -17,17 +15,16 @@ mod open_mode;
 fn ripc(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<OpenMode>()?;
     m.add_class::<PythonSharedMessage>()?;
-    m.add_class::<PythonSharedCircularQueue>()?;
     m.add_class::<PythonSharedQueue>()?;
-    
+
     m.add_function(wrap_pyfunction!(read_all, m)?)?;
     m.add_function(wrap_pyfunction!(read_all_map, m)?)?;
-    
+
     Ok(())
 }
 
 #[pyfunction]
-fn read_all(readers: Vec<Py<PythonSharedMessage>>, py: Python<'_>) -> Vec<Option<RustBytes>> {
+fn read_all(readers: Vec<Py<PythonSharedMessage>>, py: Python<'_>) -> Vec<Option<RustPyBytes>> {
     py.allow_threads(|| {
         readers
             .into_par_iter()
